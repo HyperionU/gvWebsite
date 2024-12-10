0d022bc43a20dea9563227d7ff509881eced91b1
HyperionU <108021531+HyperionU@users.noreply.github.com>
2024-12-10 18:43:02 +0000

    add adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 import sitemap from "@astrojs/sitemap";
+import vercel from '@astrojs/vercel';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  // Add your domain here
-   site: 'https://lexingtonthemes.com',
-  integrations: [ sitemap()]
+
+  integrations: [ sitemap()],
+  adapter: vercel(),
+  output: "server"
 });
