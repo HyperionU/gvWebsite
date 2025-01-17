7d97f738462ca5a1f0975202fc7559a9d15c9cda
HyperionU <108021531+HyperionU@users.noreply.github.com>
2025-01-17 03:31:11 +0000

    update build stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,5 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  // Add your domain here
-   site: 'https://lexingtonthemes.com',
-  integrations: [ sitemap()]
+  integrations: [sitemap()]
 });
