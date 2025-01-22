41e31221378e7ec2d82f8123b9e441d91e3673bb
HyperionU <108021531+HyperionU@users.noreply.github.com>
2025-01-22 22:16:06 +0000

    build: update build files

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 import sitemap from "@astrojs/sitemap";
+import react from '@astrojs/react';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [sitemap()]
+  integrations: [sitemap(), react()]
 });
